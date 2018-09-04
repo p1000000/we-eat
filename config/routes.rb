817fa47a888e8066906dad7b1f2c15f0167d05b6
Anna <anna.zeiger@wework.com>
2018-09-04 15:31:21 +0300

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  
+  resources :restaurants, only: :index
+  resources :cuisines, only: :index
+  resources :reviews, only: :create
+
+  root 'pages#index'
+
   Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     [user, password] == [ENV['SIDEKIQ_USERNAME'], ENV['SIDEKIQ_PASSWORD']]
   end
