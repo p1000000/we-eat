a7514a6a87b398e0bdbb3658acadbb55cc646dff
Dotan Shaked <dotan.shaked@wework.com>
2019-06-24 15:40:19 +0300

    basic validations to restaurant and review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
+
+  validates :name, :cuisine, presence: true
+  validates :name, uniqueness: true
+  validates :max_delivery_time, :longitude, :latitude, numericality: {greater_than_or_equal_to: 0}
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :reviewer_name, :rating, presence: true
+  validates :rating, numericality: {greater_than_or_equal_to: 0}
+
 end
